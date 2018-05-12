d90b3e20b0a221c477fa9ca66c3b931d11e27081
Jun Wang <scuwangjun@hotmail.com>
2018-05-12 19:37:33 +0800

    零钱兑换

---
+++ src/com/wangjun/leetcode/LeetCode49_.java
@@ -0,0 +1,30 @@
+package com.wangjun.leetcode;
+
+/*
+题目：零钱兑换
+给定不同面额的硬币(coins)和一个总金额(amount)。写一个函数来计算可以凑成总金额所需的最少的硬币个数。
+如果没有任何一种硬币组合方式能组成总金额，返回-1。
+
+示例 1:
+coins = [1, 2, 5], amount = 11
+return 3 (11 = 5 + 5 + 1)
+
+示例 2:
+coins = [2], amount = 3
+return -1.
+
+注意:
+
+你可以认为每种硬币的数量是无限的。
+ */
+public class LeetCode49_ {
+	public static void main(String[] args) {
+		
+	}
+
+	static class Solution {
+		public int coinChange(int[] coins, int amount) {
+	        
+	    }
+	}
+}
